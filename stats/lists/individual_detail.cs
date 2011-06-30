4b04c51cc4da4c6fcdada35235d3bf918beb0678
Christian Carter <cdcarter@gmail.com>
2011-06-29 22:46:38 -0500

    since the list func is working again, made majro headway on individual_detail.  still needs the aggregate numbers per player (and an accumulator in the loop to generate those), and i should figure out how to handle GP

--- stats/lists/individual_detail.cs
+++ stats/lists/individual_detail.cs
@@ -6,11 +6,18 @@
   
   player = []
   
+  template = "<tr><td>{{opponent}}</td><td>{{tuh}}</td><td>{{tens}}</td><td>{{negs}}</td><td>{{ppth}}</td><td>{{pts}}</td></tr>"
+  
   switch view
     when "individual"
       while row = getRow()
         split = row.key.splice(1,2)
-        if player[0] != split[0] && player[1] != split[1]
+        if player[0] != split[0] || player[1] != split[1]
           player = split
-          log(player)
+          send("</table>") unless player == []
           send("<h2><a id=#{row.id}>#{player[1]}</a>, #{player[0]}</h2>")
+          send("<table><tr><th>Opponent</th><th>TUH</th><th>10</th><th>-5</th><th>PPTH</th><th>Pts</th>")
+        
+        send(mustache.to_html template,row.value)
+        
+        
--- stats/views/individual/map.cs
+++ stats/views/individual/map.cs
@@ -2,8 +2,14 @@
   if doc.type=="game" && doc.teamAtuh && doc.teamBtuh
     for player in doc.teamAstats
       pts = ((player.tens*10)-(player.negs*5))
-      emit([doc.bracket,doc.teamA,player.name],{gp:1, tens:player.tens, negs:player.negs, tuh:player.tuh, pts:pts})
+      ppth = pts/player.tuh
+      emit([doc.bracket,doc.teamA,player.name],{
+        gp:1, tens:player.tens, ppth:ppth, 
+        negs:player.negs, tuh:player.tuh, pts:pts, opponent:doc.teamB})
 
     for player in doc.teamBstats
       pts = ((player.tens*10)-(player.negs*5))
-      emit([doc.bracket,doc.teamB,player.name],{gp:1, tens:player.tens, negs:player.negs, tuh:player.tuh, pts:pts})
+      ppth = pts/player.tuh
+      emit([doc.bracket,doc.teamB,player.name],{
+        gp:1, tens:player.tens, ppth:ppth,
+        negs:player.negs, tuh:player.tuh, pts:pts, opponent:doc.teamA})
